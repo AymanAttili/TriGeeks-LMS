5732d09c587d0d8e9278deeb67eab36672b46c19
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2024-11-06 17:35:28 +0200

    ...

--- src/features/authentication/useLogout.js
+++ src/features/authentication/useLogout.js
@@ -1,15 +1,16 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { clearTokens } from "../../utils/handleTokens";
+import { revokeRefreshToken } from "../../services/apiAuth";
 
 export function useLogout() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
   const { mutate: logout, isLoading } = useMutation({
-    mutationFn: () => {
-      clearTokens();
-      queryClient.removeQueries();
+    mutationFn: async () => {
+      await revokeRefreshToken();
+      queryClient.clear();
       navigate("/login", { replace: true });
     },
   });
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -2,10 +2,8 @@ import axiosAPI from "../API/axiosAPI";
 import { clearTokens, getAccessToken, getRefreshToken } from "../utils/handleTokens";
 import { convertToJson } from "../utils/helpers";
 
-const API_URL = import.meta.env.VITE_API_URL
-
 export async function login({ workId, password, rememberUser }) {
-    const response = await axiosAPI.post(`${API_URL}/identity/login`, {
+    const response = await axiosAPI.post(`/identity/login`, {
         workId,
         password
     }).catch(() => { throw new Error('Invalid credentials') })
@@ -14,18 +12,26 @@ export async function login({ workId, password, rememberUser }) {
     return { data, rememberUser };
 }
 
-export async function resetPassword(workId, token, newPassword) {
-    const response = await axiosAPI.post(`${API_URL}/identity/reset-forgotten-password`, {
-        workId,
-        token,
-        newPassword
+export async function revokeRefreshToken() {
+    const refreshToken = getRefreshToken();
+    const response = await axiosAPI.post(`/identity/revoke-refresh-token`, {
+        refreshToken,
     })
+    clearTokens();
 
     return response;
 }
 
+export async function resetPassword(workId, token, newPassword) {
+    return await axiosAPI.post(`/identity/reset-forgotten-password`, {
+        workId,
+        token,
+        newPassword
+    })
+}
+
 export async function forgotPassword(workId) {
-    return await axiosAPI.post(`${API_URL}/identity/forgot-password`, { workId })
+    return await axiosAPI.post(`/identity/forgot-password`, { workId })
 }
 
 export async function fetchUser() {
@@ -40,13 +46,11 @@ export async function fetchUser() {
 
     const currentDate = new Date();
 
-    console.log(expDate, currentDate);
-
     if (expDate < currentDate) {
         await refreshToken();
     }
 
-    return await axiosAPI.get(`${API_URL}/identity/users/${user?.id}`)
+    return await axiosAPI.get(`/identity/users/${user?.id}`)
 }
 
 export async function refreshToken() {
@@ -58,8 +62,13 @@ export async function refreshToken() {
 
 export async function getCurrentUser() {
     const accessToken = getAccessToken();
+    const refreshToken = getRefreshToken();
 
-    if (!accessToken) return null;
+    if (!accessToken) {
+        if (refreshToken)
+            revokeRefreshToken()
+        return null;
+    }
 
     const response = await fetchUser()
 
