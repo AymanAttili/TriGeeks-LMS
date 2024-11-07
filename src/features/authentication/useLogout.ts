17eda043ec7f751d8520972abe6dc41f4ebc9130
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2024-11-07 22:57:46 +0200

    ...

--- src/features/authentication/useLogout.ts
+++ src/features/authentication/useLogout.ts
@@ -9,9 +9,13 @@ export function useLogout() {
 
   const { mutate: logout, isLoading } = useMutation({
     mutationFn: async () => {
-      await revokeRefreshToken();
-      queryClient.clear();
-      navigate("/login", { replace: true });
+      try{
+        await revokeRefreshToken();
+      }
+      finally{
+        queryClient.clear();
+        navigate("/login", { replace: true });
+      }
     },
   });
 
